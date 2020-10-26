2c77fe7652a577f86b70f24ce21cf99d2c022ed0
Jayden LeBlanc <jaydenleblanc@gmail.com>
2020-10-25 21:10:30 -0400

    Circle plots

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,17 +1,17 @@
 
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL
-d3.json(url, function (data) {
-    console.log(data.features);
-});
+// // Perform a GET request to the query URL
+// d3.json(url, function (data) {
+//     console.log(data.features);
+// });
 
 function createMap(earthquakes) {
 
     // Create the tile layer that will be the background of our map
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
+        maxZoom: 5,
         id: "light-v10",
         accessToken: API_KEY
     });
@@ -21,9 +21,10 @@ function createMap(earthquakes) {
         "Light Map": lightmap
     };
 
-    // Create an overlaymap object to hold the earthquake
+    // Create an overlaymap object to hold the earthquakes
     var overlayMaps = {
-        "Earthquakes": earthquakes
+        "Earthquake Locations": earthquakes
+
     };
 
     // Create the map object with options
@@ -38,27 +39,92 @@ function createMap(earthquakes) {
         collapsed: false
     }).addTo(map);
 
+    // Create a legend to display information about our map
+    var info = L.control({
+        position: "bottomright"
+    });
+
+    // When the layer control is added, insert a div with the class of "legend"
+    info.onAdd = function () {
+        var div = L.DomUtil.create("div", "legend");
+        return div;
+    };
+    // Add the info legend to the map
+    info.addTo(map);
+
+
+
 }
 
 function createMarkers(response) {
     var earthquakeMarkers = [];
+    // var earthquakeMagnitudes = [];
+    console.log(response.features);
 
     for (var i = 0; i < (response.features).length; i++) {
         var lat = response.features[i].geometry.coordinates[1];
         var long = response.features[i].geometry.coordinates[0];
+        // var magnitude = response.features[i].properties.mag;
         //console.log(lat);
         //console.log(long);
-        var earthquakeMarker = L.marker([lat, long]);
+        var earthquakeMarker = L.circle([lat, long], {
+            fillOpacity: 0.75,
+            color: getColor(response.features[i].geometry.coordinates[2]),
+            fillcolor: "green",
+            radius: response.features[i].properties.mag * 1500
+        });
+        // var earthquakeMagnitude = L.marker([magnitude]);
         earthquakeMarkers.push(earthquakeMarker);
+        // earthquakeMagnitudes.push(earthquakeMagnitude);
     }
 
     createMap(L.layerGroup(earthquakeMarkers));
+    // createMap(L.layerGroup(earthquakeMagnitudes));
+
+
+    // use if statements to determine the color based on earthquakes magnitude
+
+
+
+
 };
+
+
+
+// function getRadius(magnitude) {
+//     return magnitude * 2;
+// }
+
+
+
+function getColor(density) {
+    switch (true) {
+
+        case density > 70:
+            return "red"
+        case density > 60:
+            return "red"
+        case density > 50:
+            return "green";
+        case density > 40:
+            return "green";
+        case density > 30:
+            return "yellow";
+        case density > 20:
+            return "yellow";
+        case density > 10:
+            return "pink";
+        default:
+            return "purple";
+    }
+}
+
+
 
 // use if statements to determine the color based on earthquakes magnitude
 
 
-function
+
 // // Initialize array for earthquake coordinates
 // var earthquakeCoordinates = [];
 
